2ed134f812eb3e8a53c654e6700a53edb3925df5
yeon <yeonnnnn.k@gmail.com>
2024-10-08 22:22:57 +0900

    🐾wip

---
+++ src/pages/TodaysReservation/TodaysReservation.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const S = {
+  TodaysReservation: styled.div`
+    margin: 0 auto;
+    margin-top: 80px;
+    box-sizing: border-box;
+
+    width: 1440px;
+  `,
+  StoreName: styled.header`
+    color: ${(props) => props.theme.color.green};
+    font-size: 28px;
+    font-weight: 900;
+  `,
+  UpperBox: styled.section`
+    margin-top: 8px;
+    width: 1386px;
+    height: 426px;
+    padding: 18px;
+    box-sizing: border-box;
+
+    border: 3px solid ${(props) => props.theme.color.green};
+    border-radius: 5px;
+  `,
+  Todays: styled.span`
+    font-size: 28px;
+    font-weight: 700;
+    color: ${(props) => props.theme.color.deepGreen};
+    margin-right: 14px;
+  `,
+  Date: styled.span`
+    font-weight: 600;
+    color: ${(props) => props.theme.color.deepGreen};
+  `,
+  InputBox: styled.div`
+    display: flex;
+    align-items: center;
+
+    width: 260px;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid ${(props) => props.theme.color.lightGreen};
+  `,
+  FlexBox: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+  `,
+
+  SearchIcon: styled.div`
+    margin-left: 5px;
+    width: 18px;
+    height: 18px;
+    background-image: url('/icons/searchIcon.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+  `,
+  TimelineBox: styled.div`
+    height: 326px;
+    border-radius: 5px;
+    border: 4px solid #89e46a;
+  `,
+
+  DashBoardBox: styled.section`
+    display: flex;
+    justify-content: space-between;
+
+    width: 1386px;
+    height: 404px;
+  `,
+  Card: styled.div`
+    width: 448px;
+    height: 404px;
+    background-color: ${(props) => props.theme.color.paleCoral};
+  `,
+};
---
+++ src/pages/TodaysReservation/TodaysReservation.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { S } from './TodaysReservation';
+import UserInput from '../../components/common/UserInput/UserInput.tsx';
+
+const TodaysReservation = () => {
+  return (
+    <S.TodaysReservation>
+      <S.StoreName>store Name</S.StoreName>
+      <S.UpperBox>
+        <S.FlexBox>
+          <div>
+            <S.Todays>오늘의 예약</S.Todays>
+            <S.Date>10월 07일 월요일</S.Date>
+          </div>
+          <S.InputBox>
+            <S.SearchIcon />
+            <UserInput
+              placeholder="예약자명을 입력하세요"
+              width="220px"
+              color="white"
+              height="25px"
+            />
+          </S.InputBox>
+        </S.FlexBox>
+        <S.TimelineBox></S.TimelineBox>
+      </S.UpperBox>
+      <S.DashBoardBox>
+        <S.Card />
+        <S.Card />
+        <S.Card />
+      </S.DashBoardBox>
+    </S.TodaysReservation>
+  );
+};
+
+export default TodaysReservation;

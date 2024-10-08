7bd22eb57f6254ec7dababff71c1c48993f53d84
minholeelog <91aiden.lee@kakao.com>
2024-10-08 19:27:03 +0900

    🚧 chore: 폰트 가져오는 방식 수정

--- src/styles/global/GlobalStyle.ts
+++ src/styles/global/GlobalStyle.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap');
     body {
         font-family: "Noto Sans KR", sans-serif;;
     }

66954eb64d54c97def236bcb951a74854b4abb62
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2024-10-10 11:59:16 +0900

    🐣 feat: nav router 추가

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,14 @@
 import UserInput from './common/UserInput/UserInput.tsx';
 import Nav from './common/Nav/Nav.tsx';
+import { Outlet } from 'react-router-dom';
 
 function Main() {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <Nav></Nav>Hello, World!
+      <Outlet />
+    </div>
+  );
 }
 
 export default Main;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,20 @@ function AppRouter() {
     {
       path: '/',
       element: <Main />,
+      children: [
+        {
+          path: 'dashboard',
+          element: <div>This is dashboard page</div>,
+        },
+        {
+          path: 'list',
+          element: <div>This is list page</div>,
+        },
+        {
+          path: 'store',
+          element: <div>This is store page</div>,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;

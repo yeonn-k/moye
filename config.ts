b0e5d2a2911307b77fd18711afc9033d9ef90c18
yeon <yeonnnnn.k@gmail.com>
2024-10-13 17:17:07 +0900

    🐣feat: config.ts 작성

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const URL = 'localhost:5000';
+
+export const APIS = {
+  login: `${URL}/login`,
+  signup: `${URL}/signup`,
+  today: `${URL}/today`,
+  month: `${URL}/month`,
+  owner: `${URL}/owner`,
+  store: `${URL}/store`,
+  storeEdit: `${URL}/store/edit`,
+};

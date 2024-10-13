c9986078c018362e47c3af96a4f92a2877c1ad3d
minholeelog <91aiden.lee@kakao.com>
2024-10-13 18:18:04 +0900

    🍳 refactor: api 요청 시 Authorization 헤더 적용하여 요청하도록 인터셉터 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import store from '../store/store';
 
 const URL = process.env.REACT_APP_API_URL;
 const PORT = process.env.REACT_APP_API_PORT;
@@ -6,5 +7,18 @@ const PORT = process.env.REACT_APP_API_PORT;
 const api = axios.create({
   baseURL: `${URL}:${PORT}`,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = store.getState().auth.token;
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;

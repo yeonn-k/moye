160aeffe512eca97fb46dfd17c2b8610f03e874b
yeon <yeonnnnn.k@gmail.com>
2024-10-14 10:23:00 +0900

    🐾wip

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
-export const URL = 'http://localhost:5000';
+// export const URL = 'http://localhost:5000';
 
-export const APIS = {
-  login: `${URL}/login`,
-  signup: `${URL}/signup`,
-  today: `${URL}/today`,
-  month: `${URL}/month`,
-  owner: `${URL}/owner`,
-  store: `${URL}/store`,
-  storeEdit: `${URL}/store/edit`,
-};
+// export const APIS = {
+//   login: `${URL}/login`,
+//   signup: `${URL}/signup`,
+//   today: `${URL}/today`,
+//   month: `${URL}/month`,
+//   owner: `${URL}/owner`,
+//   store: `${URL}/store`,
+//   storeEdit: `${URL}/store/edit`,
+// };

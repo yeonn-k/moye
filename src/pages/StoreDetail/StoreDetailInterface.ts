17e6c948e1861cbbf9c95ef42f86900f93c40444
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2024-10-15 21:52:48 +0900

    🐣 feat: 매장 상세 페이지 API 통신 추가

---
+++ src/pages/StoreDetail/StoreDetailInterface.ts
@@ -0,0 +1,56 @@
+interface OperatingTimeTableData {
+  type: string;
+  openFrom: string;
+  closeTo: string;
+  startBreakTime: string;
+  endBreakTime: string;
+}
+
+interface IrregularCloseDay {
+  ymd: string;
+}
+
+interface RegularHoliday {
+  closedDay: number;
+}
+
+interface StoreImage {
+  isPrimary: string;
+  src: string;
+}
+
+interface StoreDetailData {
+  openingHour: OperatingTimeTableData[];
+  closedDay: IrregularCloseDay[];
+  regularHoliday: RegularHoliday[];
+  image: StoreImage[];
+  businessRegistrationNumber: string;
+  businessName: string;
+  contact: string;
+  totalSeats: string;
+  numberPerTable: number;
+  description: string;
+  email: string;
+}
+
+const initialState: StoreDetailData = {
+  openingHour: [],
+  closedDay: [],
+  regularHoliday: [],
+  image: [],
+  businessRegistrationNumber: '',
+  businessName: '',
+  contact: '',
+  totalSeats: '',
+  numberPerTable: 0,
+  description: '',
+  email: '',
+};
+
+export type {
+  StoreDetailData,
+  OperatingTimeTableData,
+  IrregularCloseDay,
+  RegularHoliday,
+};
+export { initialState };
